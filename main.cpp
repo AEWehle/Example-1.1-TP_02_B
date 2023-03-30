f0170d6e90fe8ddf7bd4056d0801580a5f0dbb0f
Ángeles Elsa Wehle <awehle@fi.uba.ar>
2023-03-30 21:53:51 +0000

    ya pasado a c

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-
+void mode(gpio_t* gpio ,PinMode pull)
+{
+    core_util_critical_section_enter();
+    gpio_mode(gpio, pull);
+    core_util_critical_section_exit();
+}
 
 int main()
 {
     //DigitalIn gasDetector(D2);
-    gpio_init_in(&gpio, PF_15);
+    gpio_t gasDetector;
+    gpio_init_in(&gasDetector, PF_15);
 
     //DigitalOut alarmLed(LED1);
-    gpio_init_in(&gpio, PB_0);
+    gpio_t  alarmLed;
+    gpio_init_out(&alarmLed, PB_0);
 
-    gasDetector.mode(PullDown);
+    //gasDetector.mode(PullDown);
+    mode(&gasDetector,PullDown);
 
-    alarmLed = OFF;
+    //alarmLed = OFF; // off es 0
+    gpio_write(&alarmLed, 0);
 
     while (true) {
-        if ( gasDetector == ON ) {
+        //if ( gasDetector == ON ) {
+        if ( gpio_read(&gasDetector)== ON ) {
             printf("%s\n", "se detecta gas, alarma sonando");
-            alarmLed = ON;
+            //alarmLed = ON;
+            gpio_write(&alarmLed, 1);
         }
         
-        if ( gasDetector == OFF ) {
-            printf("%s\n", "no se detecta gas, alarma apagada");
-            alarmLed = OFF;
+        //if ( gasDetector == OFF ) {
+        if ( gpio_read(&gasDetector)== OFF ) {
+            printf("%s \n", "no se detecta gas, alarma apagada");
+            //alarmLed = OFF;
+            gpio_write(&alarmLed, 0);
+            //int printf(char *, ...)
         }
     }
 }
